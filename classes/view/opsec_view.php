4b6d6955d5bd1d696f05ebb6bfd4a2ae93714a56
jjwedemyer <mail@jakob-wedemyer.de>
2016-10-07 18:36:57 +0200

    opsec_view added

--- classes/view/opsec_view.php
+++ classes/view/opsec_view.php
@@ -1,1 +1,20 @@
-lol
+{% extends 'base.twig' %}
+
+{% block stylesheets %}
+{{ asset_css('selene.css') }}
+{% endblock %}
+
+{% block content %}
+<div class="content">
+  <div class="bg-danger pull-right">
+    <blockquote cite="Chuz">
+      <p class="lead">
+        Supersecret Squirrel Stuffz
+      </p>
+    </blockquote>
+    <p>
+      You are not allowed to see this content, please go back to the previous page.
+    </p>
+  </div>
+</div>
+{% endblock %}
